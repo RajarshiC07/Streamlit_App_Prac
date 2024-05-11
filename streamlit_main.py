faef6041a935b524bdf3a32a9104a40cacf6edc6
RajarshiC07 <60816446+RajarshiC07@users.noreply.github.com>
2024-05-12 02:11:15 +0530

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,43 +1,22 @@
 import streamlit as st
-import pandas as pd
-import altair as alt
-@st.cache_data
-def get_UN_data():
-    AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
+from langchain.llms import HuggingFaceHub
 
-try:
-    df = get_UN_data()
-    countries = st.multiselect(
-        "Choose countries", list(df.index), ["India", "United States of America"]
-    )
-    if not countries:
-        st.error("Please select at least one country.")
-    else:
-        data = df.loc[countries]
-        data /= 1000000.0
-        st.write("### Gross Agricultural Production ($B)", data.sort_index())
+import time
 
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
-except Exception as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e
-    )
+ENDPOINT_URL = "satvikag/chatbot"
+
+LLM = HuggingFaceHub(repo_id = ENDPOINT_URL, huggingfacehub_api_token = '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', model_kwargs={"temperature":1})
+
+with st.sidebar:
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        output = None
+        print(prompt)
+        while output is None:
+            try:
+                output = LLM.invoke(prompt)
+            except Exception as err:
+                with st.status("Typing..."):
+                    time.sleep(2)
+        messages.chat_message("assistant").write(f"Echo: {output}")
